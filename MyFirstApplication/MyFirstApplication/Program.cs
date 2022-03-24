79ac435d4d637c638022cff4bdee1a8c50ef5d6f
kdavid <kdavid@wsutech.edu>
2022-03-24 15:28:28 -0500

    CAC-103 Added MyLocalFunction()

--- MyFirstApplication/MyFirstApplication/Program.cs
+++ MyFirstApplication/MyFirstApplication/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+/*
+ * This local function is for my first exercise lab.
+ *\
+
+void MyLocalFunction()
+{
+    Console.WriteLine("This is my first sentence")
+    Console.WriteLine("I am new to CSharp")
+    Console.WriteLine("This CSharp course is cool")
+    Console.WriteLine("I am learning stuff ever")
+}
+
