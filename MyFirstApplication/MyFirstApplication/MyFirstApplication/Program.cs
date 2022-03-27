1d90fca1ceb80cdb5fc441312f4ceb2543d6f2f3
kdavid <kdavid@wsutech.edu>
2022-03-27 16:25:16 -0500

    CAC-103 Added Class and Namespace

---
+++ MyFirstApplication/MyFirstApplication/MyFirstApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace MyFirstApplication
+{
+    class Program
+    {
+        static void MyLocalFunction()
+        {
+            Console.WriteLine("This is my first sentence");
+            Console.WriteLine("I am new to CSharp");
+            Console.WriteLine("This CSharp course is cool");
+            Console.WriteLine("I am learning stuff ever");
+        }
+        static void Main(string[] args)
+        {
+            MyLocalFunction();
+        }
+
+
+    }
+}
+
+
