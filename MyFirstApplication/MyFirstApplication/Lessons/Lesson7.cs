2d78f4f1fb13c1ffa86bf33f565cbca24ac56190
kdavid <kdavid@wsutech.edu>
2022-04-12 02:58:43 -0500

    CAC-103 Records and Structs

---
+++ MyFirstApplication/MyFirstApplication/Lessons/Lesson7.cs
@@ -0,0 +1,64 @@
+﻿namespace MyFirstApplication;
+
+/*
+ A struct is a value type while a class is a reference type.
+You should use a struct for small objects that need to be
+memory dependent.
+ */
+internal struct Location
+{
+    private double _longitude, _latitude;
+
+    public double Latitude
+    {
+        get { return _latitude; }
+        set { _latitude = value; }
+    }
+
+    public double Longitude
+    {
+        get { return _longitude; }
+        set { _longitude = value; }
+    }
+
+    public Location(double longitude, double latitude)
+    {
+        _longitude = longitude;
+        _latitude = latitude;
+    }
+
+
+    // ToString Override
+    public override string ToString()
+    {
+        return $"{_longitude} - {_latitude}";
+    }
+
+} // end struct
+
+
+/* Readonly Struct */
+public readonly struct Student
+{
+    private readonly int _id;
+    private readonly string _name;
+
+    public int StudentId
+    {
+        get { return _id; }
+        // set { _id = value; }
+    }
+
+    public string StudentName
+    {
+        get { return _name; }
+        // set { _name = value; }
+    }
+
+    public Student(int studentId, string studentName)
+    {
+        _id = studentId;
+        _name = studentName;
+    }
+
+} // end struct
---
+++ MyFirstApplication/MyFirstApplication/Lessons/Lesson7Record.cs
@@ -0,0 +1,19 @@
+﻿namespace MyFirstApplication;
+
+
+/*
+ A Record is a reference type that provides built-in functionality
+for encapsulating data
+ */
+internal record Person(string firstName, string lastName);
+
+/*
+ A Record struct is a value type with similar functionality as a record class
+ */
+public record struct Resolution(int width, int height)
+{
+    public void CalculateRes()
+    {
+        Console.WriteLine(width * height);
+    }
+} // end record
