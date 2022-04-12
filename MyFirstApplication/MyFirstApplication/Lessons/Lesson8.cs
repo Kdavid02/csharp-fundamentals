7d59ea5ff7406a1e83b9706d8a74e17d9e93ae46
kdavid <kdavid@wsutech.edu>
2022-04-12 04:09:51 -0500

    CAC-103 Lesson 8 Constants and Static

---
+++ MyFirstApplication/MyFirstApplication/Lessons/Lesson8.cs
@@ -0,0 +1,43 @@
+﻿namespace MyFirstApplication;
+
+/*
+Constants and Static 
+ */
+internal class Lesson8
+{
+    // Constant variables
+    public const int Months = 12; // per year
+    public const int Weeks = 52; // per year
+    public const int Days = 365; // per year
+    public const int Hours = 60; // per day
+
+    public const double DaysPerMonth = (double)Days / (double)Months;
+
+    public static int DayCount;
+    private int _days;
+
+    public Lesson8(int calDays)
+    {
+        _days = calDays;
+    }
+
+    public static int CalculateDayCount()
+    {
+        return ++DayCount;
+    }
+
+    public void CalculateHours()
+    {
+        int total = Hours * _days;
+        Console.WriteLine($"The total hours for {_days} days is {total}.");
+        CalculateDayCount();
+    }
+
+    public void CalculateHoursV2()
+    {
+        Func<int, int> totalHours = static value => Hours * value;
+        Console.WriteLine($"The total version2 hours for {_days} is " +
+            $"{totalHours(_days)}");
+    }
+
+} // end class
---
+++ MyFirstApplication/MyFirstApplication/Lessons/Lesson8Static.cs
@@ -0,0 +1,21 @@
+﻿namespace MyFirstApplication;
+
+/*
+ Static class must have static methods as you can not use instance methods.
+ */
+internal static class Lesson8Static
+{
+    public static double CelsiusToFahrenheit(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double FahrenheitToCelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+
+
+
+} // end class
