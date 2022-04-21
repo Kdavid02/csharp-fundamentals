c2de67c81e61784896bc4c20cda90c1ea1fbfb5f
kdavid <kdavid@wsutech.edu>
2022-04-20 23:36:47 -0500

    CAC-103 Exercise 10

--- MyFirstApplication/MyFirstApplication/Exercises/Boat.cs
+++ MyFirstApplication/MyFirstApplication/Exercises/Boat.cs
@@ -5,12 +5,16 @@ internal class Boat_Object // boat was used already in the namespace
 {
     /*
     Problem 2. Create a class based on your Boat_Object object. You will follow the same
+    
     instructions as exercise 1 except for the following.
+    
     Include your attributes as Expression Body Definition Properties
     */
 
     public string paintcolor;
+
     public int sailsize;
+
     public string sailShape;
 
     public string PaintColor
