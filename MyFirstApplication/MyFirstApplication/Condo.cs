07b99c67676ddb792517ace72ebd6daa193520cf
kdavid <kdavid@wsutech.edu>
2022-04-21 17:25:17 -0500

    CAC-103 Lesson 12 Polymorphism

---
+++ MyFirstApplication/MyFirstApplication/Condo.cs
@@ -0,0 +1,38 @@
+﻿namespace MyFirstApplication;
+
+internal class Condo : House
+{
+    private string _balcony;
+
+    public string Balcony
+    {
+        get { return _balcony; }
+        set { _balcony = value; }
+    }
+
+    public Condo()
+        :this("second floor")
+    { }
+
+    public Condo(string balcony)
+        : this("Concrete", "steel", "Blue", 35, balcony)
+    { }
+
+    public Condo(string foundation, string roofType, string doorColor,
+        int windowSize, string balcony)
+        : base(foundation, roofType, doorColor, windowSize)
+    {
+        _balcony = balcony;
+    }
+
+    public override void DoorOpenClose()
+    {
+        Console.WriteLine($"The {DoorColor} door is creaking.");
+    }
+
+    public void Maintenance()
+    {
+        Console.WriteLine($"Fixing the {_balcony} on appartment 2.");
+    }
+
+} // end class
---
+++ MyFirstApplication/MyFirstApplication/Lessons/Lesson12.cs
@@ -0,0 +1,30 @@
+﻿namespace MyFirstApplication;
+
+// Base class with a method declared virtual for overriding
+public class Fruit
+{
+    public virtual void EatFruit()
+    {
+        Console.WriteLine("I eat fruit.");
+    }
+
+    public void EatFruit(int quantity)
+    {
+        Console.WriteLine($"I eat {quantity} portions of fruit everyday.");
+    }
+
+} // end class
+
+
+
+// Derived class overriding the base class method
+public class Apple : Fruit
+{
+    public override void EatFruit()
+    {
+        base.EatFruit();
+        Console.WriteLine("I eat apples.");
+    }
+
+
+} // end class
