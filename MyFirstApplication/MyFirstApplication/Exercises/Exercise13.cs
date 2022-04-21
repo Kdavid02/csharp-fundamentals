b770c883996486e5d573fd69e7c3a1315a4936b3
kdavid <kdavid@wsutech.edu>
2022-04-21 18:12:12 -0500

    CAC-103 Exercise 13

---
+++ MyFirstApplication/MyFirstApplication/Exercises/Exercise13.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace MyFirstApplication;
+
+/*
+ This class covers the content for Exercise13
+ */
+
+/*
+Problem 1 - In your Exercise13.cs file, change your class to an Interface and name it 
+IMovement. E
+This interface will have 1 property. The property will an int 
+and called Speed. It will have a get and init.
+ */
+internal interface IMovement
+{
+    int Speed { get; init; }
+
+} 
+
+/*
+Problem 2 - Under the interface you will create an Abstract class called WaterBirds. 
+This abstract class will implement the IMovement interface. The interface 
+property should be implemented in the Abstract class as an abstract 
+property. Create two abstract methods for the abstract class that is related 
+to WaterBirds. Create a constructor that will take an int parameter and be 
+assigned to the Speed property.
+ */
+internal abstract class WaterBirds : IMovement
+{
+    public abstract int Speed { get; init; }
+
+    public abstract void Tweet();
+    public abstract void FlapWings();
+
+    public WaterBirds(int speed)
+    {
+        this.Speed = speed;
+    }
+}
+
+/*
+Problem 3 - Under the Abstract class Waterbirds, create a derived class that will extend 
+the WaterBird class. 
+Implement the abstract methods and property. Use 
+Console Writeline statements in your methods that provide information 
+related to the methods. One of them should use an Interpolation string and 
+consume the Speed property. Create a constructor that will take an int 
+parameter and use the :base() to pass that int value back to the base 
+Abstract class
+ */
+internal class Pelican : WaterBirds
+{
+    public override int Speed { get; init; } = 3;
+
+    public override void Tweet()
+    {
+        Console.WriteLine($"The Water Bird tweets it's simple but sweet song!");
+    }
+
+    public override void FlapWings()
+    {
+        Console.WriteLine($"The bird flies at the speed of {Speed} mph across the ocean");
+    }
+    // part of problem 3 
+    
+    public Pelican(int speed)
+        : base(speed) { }
+
+} // end class
---
+++ MyFirstApplication/MyFirstApplication/Lessons/Lesson13.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace MyFirstApplication;
+
+/*
+ * 
+ This will be about Abstract modifier 
+    and Interfaces
+ */
+public interface ILesson13Interface
+{
+    int Miles { get; }
+    int ConvertMilesToYards();
+} // end interface
+
+internal abstract class Lesson13Base : ILesson13Interface
+{
+    protected int num;
+
+    // Abstract properties
+    protected abstract int Inches { get; }
+    protected abstract int Feet { get; }
+
+    // Implemented from interface
+    public abstract int Miles { get; }
+
+
+    protected Lesson13Base(int num)
+    {
+        this.num = num;
+    }
+
+    // Abstract Methods
+     public abstract int ConvertFeetToInches();
+    public abstract int ConvertYardsToFeet();
+
+    // implemented from interface
+    public abstract int ConvertMilesToYards();
+
+
+} // end class
+
+/*
+ Our derived class will implement 
+ our Abstract class members
+ */
+internal class Lesson13Derived : Lesson13Base
+{
+    // implementing abstract properties
+
+    protected override int Inches { get; } = 12;
+    protected override int Feet { get; } = 3;
+
+    // implementing interface
+    public override int Miles { get; } = 1760;
+
+    public Lesson13Derived(int value)
+        : base(value) { }
+
+    // implemented abstract methods
+    public override int ConvertFeetToInches()
+    {
+        return num * Inches;
+    }
+
+    public override int ConvertYardsToFeet()
+    {
+        return num * Feet;
+    }
+
+    // implemented interface method
+    public override int ConvertMilesToYards()
+    {
+        return num * Miles;
+    }
+
+} // end class
