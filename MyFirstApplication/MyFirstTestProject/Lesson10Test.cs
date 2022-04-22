bb6e77d33c3b078bebf3588cc0890642af4ec820
kdavid <kdavid@wsutech.edu>
2022-04-21 23:09:24 -0500

    CAC-103 Exercise 18

--- MyFirstApplication/MyFirstTestProject/Lesson10Test.cs
+++ MyFirstApplication/MyFirstTestProject/Lesson10Test.cs
@@ -25,6 +25,43 @@ namespace MyFirstTestProject
 
         }
 
+
+        /*
+        Lesson 18 Lab Exercise - Unit Test & the Test Project
+        
+        Problem 1 - Write a test method inside the Lesson10Test.cs file. Ensure you have the 
+        [TestMethod] above this method. 
+        This method will be called IdVerify. We 
+        will be utilizing the arrange and assert portions of the AAA pattern. Create a 
+        variable of int called id. 
+        Provide a value for this variable. Create an object of 
+        Lesson 10 using the default constructor. 
+        Using the object created, assign 
+        the id variable to the Id property. Using an Assert, we are going to check to 
+        see if the id property was assigned properly, or if its still using the default 
+        value of 1000. 
+        Pick through the Assert methods to determine which would 
+        fit. 
+        There are more than 1 possible method to test this, including the one 
+        used in the lecture. Run the test several times to ensure you get both a 
+        passing test and a failure. Ensure you provide a passing version in your 
+        checked in code. You only need to provide 1 Assert + method
+         */
+
+        [TestMethod]
+         public void IdVerify()
+        {
+
+            // Arrange check
+            int id = 18;
+            Lesson10 lesson10 = new Lesson10();
+            lesson10.Id = id; 
+
+            // act check not present
+
+            // Assert check
+            Assert.AreEqual(id, lesson10.Id);
+        }
         
     }
 }
